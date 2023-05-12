83700541fafa4ed7a36f54dc6b8b8a2e28daeaea
manoel batista <manoel.viatech@gmail.com>
2023-05-12 10:32:02 -0300

    implementações

--- __init__.py
+++ __init__.py
@@ -3,3 +3,5 @@ from SaveMetaData import SaveMetaData
 from SelectionSort import SelectionSort
 from BubbleSort import bubble
 from InsertionSort import InsertionSort
+from TimSort import TimSort
+from HeapSort import HeapSort
